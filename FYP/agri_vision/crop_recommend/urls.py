0ba0d3a5016a62d6a6e4adcc0c9995db34c4050d
Lohith Kumar CM <134734620+LohithKumarCM@users.noreply.github.com>
2025-04-15 11:17:31 +0530

    Add files via upload

--- FYP/agri_vision/crop_recommend/urls.py
+++ FYP/agri_vision/crop_recommend/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.recommend_crop, name='recommend_crop'),
+    path("", views.recommend_crop, name="recommend_crop"),
 ]
--- FYP/agri_vision/crop_recommend/views.py
+++ FYP/agri_vision/crop_recommend/views.py
@@ -2,24 +2,32 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import joblib
 
+
+
 def recommend_crop(request):
     if request.method == "POST":
         try:
             data = request.POST
-            nitrogen = float(data['nitrogen'])
-            phosphorus = float(data['phosphorus'])
-            potassium = float(data['potassium'])
-            ph = float(data['ph'])
-            rainfall = float(data['rainfall'])
-            temperature = float(data['temperature'])
-            humidity = float(data['humidity'])
+            nitrogen = float(data["nitrogen"])
+            phosphorus = float(data["phosphorus"])
+            potassium = float(data["potassium"])
+            ph = float(data["ph"])
+            rainfall = float(data["rainfall"])
+            temperature = float(data["temperature"])
+            humidity = float(data["humidity"])
 
-            # Load ML model and predict
-            model = joblib.load(r'C:\Users\Lkumar\OneDrive - OPENMYNZ SOFTLABS PRIVATE LIMITED\Desktop\FYP\Crop_Recommend.pkl')
-            prediction = model.predict([[nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall]])
+                # Local path (replace with your actual local path)
+            model_path = r"path to your local model/Crop_Recommend.pkl"
 
-            return JsonResponse({'recommended_crop': prediction[0]})
+            # Load the ML model
+            model = joblib.load(model_path)
+            prediction = model.predict(
+                [[nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall]]
+            )
+
+            return JsonResponse({"recommended_crop": prediction[0]})
+
         except Exception as e:
-            return JsonResponse({'error': str(e)}, status=400)
+            return JsonResponse({"error": str(e)}, status=400)
 
-    return render(request, 'crop_recommend/crop_recommend.html')
+    return render(request, "crop_recommend/crop_recommend.html")
